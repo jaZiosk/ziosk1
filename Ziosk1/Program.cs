ef7ed3f8f265922abf2070d67fb69ae6e9106ccb
jaZiosk <57096659+jaZiosk@users.noreply.github.com>
2019-10-28 10:56:26 -0500

    First commit

---
+++ Ziosk1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ZioskProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            while (true)
+            {
+                Console.WriteLine("Enter zip code or 'exit' to quit : ");
+                string readStr = Console.ReadLine();
+                if (readStr == "exit")
+                {
+                    break;
+                }
+
+                try
+                {
+                    
+                    using (TextFieldParser lines = new TextFieldParser(path+ @"/data/LA_Full_Service_Restaurants.csv"))
+                    {
+
+                        while (true)
+                        {
+                            lines.Delimiters = new string[] { "," };
+
+                            string[] parts = lines.ReadFields();
+                            if (parts == null)
+                            {
+                                break;
+                            }
+
+                            int count = 0;
+                            if (parts[5].Substring(0, 5) == readStr)
+                            {
+                                Console.Write(parts[1]);
+                                Console.Write(" - ");
+                                Console.WriteLine(parts[3]);
+                                count++;
+                            }
+                            if (count >= 5)
+                                break;
+                        }
+                    }
+                    Console.WriteLine("Above are the first 5 restaurants in the given zip code.\n");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Internal server error : \n" + ex.Message);
+                }
+
+
+            }
+        }
+    }
+}
+
